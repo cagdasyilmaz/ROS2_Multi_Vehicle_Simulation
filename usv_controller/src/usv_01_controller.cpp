5aaac925a400aa87a4ebd6c783397f0d7a5626aa
cagdasyilmaz <cagdasyilmaz@icloud.com>
2024-03-12 01:57:08 +0300

    Changes to be committed: 	modified:   usv_controller/src/usv_01_controller.cpp

--- usv_controller/src/usv_01_controller.cpp
+++ usv_controller/src/usv_01_controller.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /****************************************************************************
  * MIT License
  *
@@ -24,8 +23,6 @@
  *
  ****************************************************************************/
 
-=======
->>>>>>> 0d5a5bc81ccbd42e5ae363f0fef6c230d54edc0c
 #include "../include/subscriptions/odometry.hpp"
 
 #include <rclcpp/rclcpp.hpp>
@@ -41,28 +38,28 @@ using std::placeholders::_1;
 class PublisherNode : public rclcpp::Node
 {
 public:
-	PublisherNode(const std::string& node_name, const std::shared_ptr<SubscriberNode>& subscriber_node, const std::string& topic_name)
-    	: Node(node_name), subscriber_node_(subscriber_node)
+    PublisherNode(const std::string& node_name, const std::shared_ptr<SubscriberNode>& subscriber_node, const std::string& topic_name)
+        : Node(node_name), subscriber_node_(subscriber_node)
     {
-		publisher_ = create_publisher<geometry_msgs::msg::Twist>(topic_name, 10);
+        publisher_ = create_publisher<geometry_msgs::msg::Twist>(topic_name, 10);
         timer_ = create_wall_timer(50ms, std::bind(&PublisherNode::timer_callback, this));
     }
 
     void timer_callback()
     {
-    	auto position = subscriber_node_->get_position();
+        auto position = subscriber_node_->get_position();
 
-    	if(position.x < 10.0f)
-    		message_.linear.x = 0.10;
-    	else
-    		message_.linear.x = 0.0;
+        if(position.x < 10.0f)
+            message_.linear.x = 0.10;
+        else
+            message_.linear.x = 0.0;
 
-    	message_.linear.y  = 0.0;
+        message_.linear.y  = 0.0;
         message_.linear.z  = 0.0;
-    	message_.angular.x = 0.0;
-    	message_.angular.y = 0.0;
-    	message_.angular.z = 0.0;
-    	publisher_->publish(message_);
+        message_.angular.x = 0.0;
+        message_.angular.y = 0.0;
+        message_.angular.z = 0.0;
+        publisher_->publish(message_);
     }
 
 private:
@@ -77,7 +74,6 @@ int main(int argc, char * argv[])
     rclcpp::init(argc, argv);
 
     std::vector<std::shared_ptr<SubscriberNode>> fleet_info;
-<<<<<<< HEAD
 
     fleet_info.push_back(std::make_shared<SubscriberNode>("usv_01_controller_sub", "/box_bot1/odom"));
     fleet_info.push_back(std::make_shared<SubscriberNode>("usv_02_controller_sub", "/box_bot2/odom"));
@@ -92,41 +88,20 @@ int main(int argc, char * argv[])
     while (rclcpp::ok())
     {
 
-=======
+        for(auto vehicle_info : fleet_info)
+        {
+            rclcpp::spin_some(vehicle_info);
+        }
+        rclcpp::spin_some(node_pub);
 
-    fleet_info.push_back(std::make_shared<SubscriberNode>("usv_01_controller_sub", "/box_bot1/odom"));
-    fleet_info.push_back(std::make_shared<SubscriberNode>("usv_02_controller_sub", "/box_bot2/odom"));
-    fleet_info.push_back(std::make_shared<SubscriberNode>("usv_03_controller_sub", "/box_bot3/odom"));
-    fleet_info.push_back(std::make_shared<SubscriberNode>("usv_04_controller_sub", "/box_bot4/odom"));
-    fleet_info.push_back(std::make_shared<SubscriberNode>("usv_05_controller_sub", "/box_bot5/odom"));
-    fleet_info.push_back(std::make_shared<SubscriberNode>("usv_06_controller_sub", "/box_bot6/odom"));
+        std::vector<Position> vehicle_positions;
 
-    auto node_pub = std::make_shared<PublisherNode>("usv_01_controller_pub", fleet_info[0], "/box_bot1/cmd_vel");
+        for(auto vehicle_info : fleet_info)
+        {
+            vehicle_positions.push_back(vehicle_info->get_position());
+        }
 
-    // Manually spinning nodes with a while loop
-    while (rclcpp::ok())
-    {
-
->>>>>>> 0d5a5bc81ccbd42e5ae363f0fef6c230d54edc0c
-    	for(auto vehicle_info : fleet_info)
-    	{
-    		rclcpp::spin_some(vehicle_info);
-    	}
-    	rclcpp::spin_some(node_pub);
-
-    	std::vector<Position> vehicle_positions;
-
-    	for(auto vehicle_info : fleet_info)
-    	{
-    		vehicle_positions.push_back(vehicle_info->get_position());
-    	}
-
-<<<<<<< HEAD
         //printf("[Boat1] [x, y]: [%.2f, %.2f]\n", vehicle_positions[0].x, vehicle_positions[0].y);
-=======
-        printf("[Boat1] [x, y]: [%.2f, %.2f]\n", vehicle_positions[0].x, vehicle_positions[0].y);
-
->>>>>>> 0d5a5bc81ccbd42e5ae363f0fef6c230d54edc0c
 
         std::this_thread::sleep_for(200ms); // Add a small delay to reduce CPU usage
     }
